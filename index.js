68d00a14a2ee3f786d9dd1b0d11338cdd63b44a2
Syed Samiuddin <syedsamiuddin@Syeds-MacBook-Pro.local>
2020-02-12 01:18:05 +0530

    fix...2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   })
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT} port`)
 })
